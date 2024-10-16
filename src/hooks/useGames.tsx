a73f68a230cfccc7c7379840ad3e1f2c773dd5da
Saleem Ali <saleema@clavax.com>
2024-10-16 22:11:45 +0530

    reusable api client

--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -1,8 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./userPlatforms";
 
+const apiClient = new APIClient<Game>('/games')
 
 export interface Game {
   id: number;
@@ -31,14 +32,14 @@ export interface Game {
 
 const useGames = (gameQuery: GameQuery) => useQuery<FetchResponse<Game>, Error>({
   queryKey: ['games', gameQuery],
-  queryFn: () => apiClient.get<FetchResponse<Game>>("/games", {
+  queryFn: () => apiClient.getAll({
     params: {
       genres: gameQuery.genre?.id,
       parent_platforms: gameQuery.platform?.id,
       ordering: gameQuery.sortOrder,
       search: gameQuery.searchText
     }
-  }).then(res => res.data)
+  }),
 })
 
 export default useGames;
--- src/hooks/useGenre.tsx
+++ src/hooks/useGenre.tsx
@@ -1,7 +1,10 @@
 // import useData from "./useData";
 import { useQuery } from "@tanstack/react-query";
 import genres from "../data/genres";
-import apiClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
+
+const apiClient = new APIClient<Genre>('/genres')
+
 export interface Genre {
   id: number;
   name: string;
@@ -13,7 +16,7 @@ export interface Genre {
 
 const useGenre = () => useQuery({
   queryKey: ['genres'],
-  queryFn: () => apiClient.get<FetchResponse<Genre>>('/genres').then(res => res.data),
+  queryFn: apiClient.getAll,
   staleTime: 24 * 10 * 10 * 1000, //24h
   initialData: { count: genres.length, results: genres }
 })
