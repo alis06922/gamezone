b3173fcb8e33dcf452c412cb3a6e1fb27fc34c77
Saleem Ali <saleema@clavax.com>
2024-11-16 22:48:06 +0530

    search bar game count

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,12 +3,25 @@ import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs'
 import useGameQueryStore from '../store';
 import { useNavigate } from 'react-router-dom';
+import useGames from '../hooks/useGames';
+import { useState, useEffect } from 'react'
 
 
 function SearchInput() {
     const setSearchText = useGameQueryStore(s => s.setSearchText);
     const ref = useRef<HTMLInputElement>(null);
     const navigate = useNavigate();
+    const { data } = useGames();
+    const [initialGameCount, setInitialGameCount] = useState<string | null>(null);
+    useEffect(() => {
+        if (data && !initialGameCount) {
+            const fetchDataLength = data.pages[0].count;
+            setInitialGameCount(fetchDataLength ? fetchDataLength.toLocaleString() : null);
+        }
+    }, [data, initialGameCount]);
+
+    const placeholder_text = initialGameCount ? `Search ${initialGameCount} games` : 'Search games';
+
     return (
         <form onSubmit={(event) => {
             event.preventDefault();
@@ -19,7 +32,7 @@ function SearchInput() {
         }}>
             <InputGroup>
                 <InputLeftElement children={<BsSearch />} />
-                <Input ref={ref} borderRadius={20} placeholder='Search games...' variant='filled' />
+                <Input ref={ref} borderRadius={20} placeholder={placeholder_text} variant='filled' />
             </InputGroup>
         </form>
     )
